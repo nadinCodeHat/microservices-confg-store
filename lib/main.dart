70197ac9704cb0145bff38615bf0e579c3eafa9e
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2021-12-10 23:21:48 +0530

    StatelessWidget for main.dart -> call HomePage Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,20 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:real_time_object_detection/homepage.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Real Time Object Detection App',
-      theme: new ThemeData(
+      theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'ML App'),
+      home: HomePage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _ButtonState createState() => new _ButtonState();
-}
-
-/*
-class _ButtonState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-              onPressed: () {
-                // Respond to button press
-              },
-              child: Text('TEXT BUTTON'),
-            ),
-            SizedBox(height: 8.0),
-            OutlinedButton(
-              onPressed: () {
-                // Respond to button press
-              },
-              child: Text("OUTLINED BUTTON"),
-            ),
-            SizedBox(height: 8.0),
-            ElevatedButton(
-              onPressed: () {
-                // Respond to button press
-              },
-              child: Text("CONTAINED BUTTON"),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-*/
-
-class _ButtonState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-        body: Padding(
-      padding: EdgeInsets.all(10),
-      child: Row(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(15),
-            child: new ButtonBar(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                new ElevatedButton(
-                  child: new Text('Object Detector'),
-                  style: ButtonStyle(
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.white),
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.blueAccent),
-                  ),
-                  onPressed: () {},
-                ),
-                new ElevatedButton(
-                  child: new Text('Handwritten Digit Classifier'),
-                  style: ButtonStyle(
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.white),
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.blueAccent),
-                  ),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    ));
   }
 }
